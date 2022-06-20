afe8b71a95094a45b432e1076dddf3e6c730d4c4
Stephen Molloy <stephen.molloy@maxiv.lu.se>
2022-06-20 14:23:06 +0200

    Ton of changes....

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use std::io::stdout;
+use std::fs::File;
+use std::io::{stdout, BufRead, BufReader};
 
 use crossterm::{
     cursor::MoveTo,
     event::{read, Event, KeyCode, KeyEvent},
-    style::{style, Color, PrintStyledContent, ResetColor, Stylize},
+    style::{style, Attribute, Color, PrintStyledContent, ResetColor, Stylize},
     terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType},
     ExecutableCommand, Result,
 };
@@ -13,42 +14,66 @@ enum Tab {
     DONE,
 }
 
+impl Tab {
+    fn toggle(&self) -> Self {
+        match self {
+            Tab::TODO => Tab::DONE,
+            Tab::DONE => Tab::TODO,
+        }
+    }
+}
+
 fn main() -> Result<()> {
-    let mut std = stdout();
-    enable_raw_mode().unwrap();
+    let lines: Vec<String> =
+        BufReader::new(File::open("/home/smolloy/.config/todo_rust/items").expect("no such file"))
+            .lines()
+            .map(|l| l.expect("Could not parse line"))
+            .collect();
 
-    let todos = vec!["Bake bread", "Buy things", "Write a TODO app"];
-    let dones = vec![
-        "Run 5 km",
-        "Email FemtoMAX",
-        "Check downtimes",
-        "Figure out the thing",
-    ];
+    let mut todos: Vec<&str> = lines
+        .iter()
+        .filter(|s| s.starts_with("TODO: "))
+        .map(|s| s.strip_prefix("TODO: ").unwrap() as &str)
+        .collect();
+    let mut dones: Vec<&str> = lines
+        .iter()
+        .filter(|s| s.starts_with("DONE: "))
+        .map(|s| s.strip_prefix("DONE: ").unwrap() as &str)
+        .collect();
     let mut curr_item = 0;
-
-    let mut fg_color: Color;
-    let mut bg_color: Color;
 
     let mut tab = Tab::TODO;
 
+    let mut stdout = stdout();
+    enable_raw_mode().unwrap();
+    let mut fg_color: Color;
+    let mut bg_color: Color;
+
     loop {
-        std.execute(Clear(ClearType::All))?.execute(MoveTo(0, 0))?;
+        stdout
+            .execute(Clear(ClearType::All))?
+            .execute(MoveTo(0, 0))?;
 
         let items;
         let title;
+        let prefix;
         match tab {
             Tab::TODO => {
                 title = "[TODO] DONE";
-                items = &todos;
+                items = todos.clone();
+                prefix = " [ ] :: ";
             }
             Tab::DONE => {
                 title = " TODO [DONE]";
-                items = &dones;
+                items = dones.clone();
+                prefix = " [X] :: ";
             }
         }
 
-        std.execute(PrintStyledContent(style(format!("{}\n\r", title))))?;
-        for (ind, todo) in items.iter().enumerate() {
+        stdout.execute(PrintStyledContent(
+            style(format!("{}\n\r", title)).attribute(Attribute::Bold),
+        ))?;
+        for (ind, item) in items.iter().enumerate() {
             if ind == curr_item {
                 fg_color = Color::Black;
                 bg_color = Color::White;
@@ -56,8 +81,10 @@ fn main() -> Result<()> {
                 fg_color = Color::White;
                 bg_color = Color::Black;
             }
-            std.execute(PrintStyledContent(
-                style(format!("{}\n\r", todo)).with(fg_color).on(bg_color),
+            stdout.execute(PrintStyledContent(
+                style(format!("{}{}\n\r", prefix, item))
+                    .with(fg_color)
+                    .on(bg_color),
             ))?;
         }
 
@@ -85,16 +112,36 @@ fn main() -> Result<()> {
                 code: KeyCode::Tab, ..
             }) => {
                 curr_item = 0;
-                match tab {
-                    Tab::DONE => tab = Tab::TODO,
-                    Tab::TODO => tab = Tab::DONE,
-                }
+                tab = tab.toggle();
             }
+            Event::Key(KeyEvent {
+                code: KeyCode::Enter,
+                ..
+            }) => match tab {
+                Tab::TODO => {
+                    dones.push(todos.remove(curr_item));
+                }
+                Tab::DONE => {
+                    todos.push(dones.remove(curr_item));
+                }
+            },
+            Event::Key(KeyEvent {
+                code: KeyCode::Char('d'),
+                ..
+            }) => match tab {
+                Tab::TODO => {
+                    todos.remove(curr_item);
+                }
+                Tab::DONE => {
+                    dones.remove(curr_item);
+                }
+            },
             _ => (),
         }
     }
 
     disable_raw_mode().unwrap();
-    std.execute(ResetColor)?;
+    stdout.execute(ResetColor)?;
+
     Ok(())
 }
